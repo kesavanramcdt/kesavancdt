e87b744486b34f813e029b931aa67de5b17208cb
kesavanramakrishnan <69773905+kesavanramakrishnan@users.noreply.github.com>
2023-06-23 11:43:32 -0700

    new updates

--- state-template/index.mjs
+++
@@ -1,7 +0,0 @@
-const error = () => {
-  throw new Error(
-    "Please import appropriate files separately. See Docs -> https://www.npmjs.com/package/@cagovweb/state-template"
-  );
-};
-
-export default { er: error() };
